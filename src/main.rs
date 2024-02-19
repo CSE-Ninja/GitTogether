d9c2e27dfb1eb915b6fcdf4feb0a2c865cfdf425
Ao Li <leeleo3x@gmail.com>
2024-02-19 10:58:33 -0500

    update main.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn main() {
     let periods = if let Some(period_str) = args.period {
         parse_from_input(&period_str)
     } else {
-        get_recent_one_month()
+        parse_from_input("Project 2(S1)/2024-01-20T00:00:00-05:00/2024-02-13T23:59:00-05:00;Project 2(S2)/2024-02-14T00:00:00-05:00/2024-02-29T23:59:00-05:00")
     };
 
     let svg = process(&args.repo, periods, &args.style).await;
